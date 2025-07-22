b340743436d252d5d9cb9fb6397d80988b5ea113
Dlfmiee <aidilsahimat07@gmail.com>
2025-07-22 12:17:48 +0800

    Made a change in lab03

--- labs/lab03/lab-03-2.py
+++ labs/lab03/lab-03-2.py
@@ -1,13 +1,6 @@
-Function Main
-    Declare Integer NUMNIGHT
-
-    Input NUMNIGHT
-    Declare Real TOTALPAYMENT
-
-    If NUMNIGHT > 0
-        Assign TOTALPAYMENT = (NUMNIGHT * 250 * 0.15)+250
-    Else
-        Assign TOTALPAYMENT = 0
-    End
-    Output TOTALPAYMENT
-End
+nUMNIGHT = int(input())
+if nUMNIGHT > 0:
+    tOTALPAYMENT = nUMNIGHT * 250 * 0.15 + 250
+else:
+    tOTALPAYMENT = 0
+print(tOTALPAYMENT)
