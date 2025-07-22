8b1d02c2d233a42f1f847b22772dcab6012814e4
Dlfmiee <aidilsahimat07@gmail.com>
2025-07-22 13:06:31 +0800

    Made a change in lab03

--- labs/lab03/lab-03-8.py
+++ labs/lab03/lab-03-8.py
@@ -1,7 +1,7 @@
 principal = float(input())
 rate = float(input())
 time = float(input())
-interest = principal * rate * time
+interest = principal + principal * rate * time / 100
 totalAmount = principal * 1 + rate / 100
 monthlyInterest = principal + principal * rate * time * 12 / 100 * 12
 print(interest)
