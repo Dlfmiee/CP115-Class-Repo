fdb45732bf4e33a59cb7ba21128e8a3f11765d14
Dlfmiee <aidilsahimat07@gmail.com>
2025-07-22 13:09:05 +0800

    Made a change in lab03

--- labs/lab03/lab-03-6.py
+++ labs/lab03/lab-03-6.py
@@ -1,22 +1,9 @@
-import sys
-
-def main():
-    try:
-        # Read input from the test framework
-        values = list(map(float, sys.stdin.read().split()))
-        if len(values) != 4:
-            return []
-
-        yardLength, yardWidth, houseLength, houseWidth = values
-        areaOfYard = yardLength * yardWidth
-        areaOfHouse = houseLength * houseWidth
-        builtInHouseArea = areaOfYard - areaOfHouse
-        wage = builtInHouseArea * 2
-
-        return [wage]
-    except:
-        return []
-
-if __name__ == "__main__":
-    result = main()
-    print(result)
+yardLength = float(input())
+yardWidth = float(input())
+houseLength = float(input())
+houseWidth = float(input())
+areaOfYard = yardLength * yardWidth
+areaOfHouse = houseLength * houseWidth
+builtInHouseArea = areaOfYard - areaOfHouse
+wage = builtInHouseArea * 2
+print(wage)
