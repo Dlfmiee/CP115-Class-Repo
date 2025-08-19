005c4f8a400c739ffa5ecdfb19569f51920d6c42
Dlfmiee <aidilsahimat07@gmail.com>
2025-08-19 12:53:25 +0800

    Made a change in lab7

--- labs/lab07/exercise3/exercise3.py
+++ labs/lab07/exercise3/exercise3.py
@@ -1,12 +1,12 @@
 # import
-import my_data
+import school_data
 import random
 import math
 
-print(f"Student: {my_data.student_name}")
-print(f"Student ID: {my_data.student_id}")
-print(f"Age: {my_data.student_age}")
-print(f"Course: {my_data.course_code} - {my_data.course_name}")
+print(f"Student: {school_data.student_name}")
+print(f"Student ID: {school_data.student_id}")
+print(f"Age: {school_data.student_age}")
+print(f"Course: {school_data.course_code} - {school_data.course_name}")
 
 random_number = random.randint(70, 95)
 print(f"Random Score 1 : {random_number}")
@@ -16,9 +16,9 @@ print(f"Random score 2 : {random_number2}")
 Total = random_number + random_number2
 print(f"Total Score: {Total}")
 
-print(f"Name in UpperCase: {my_data.name_upper}")
-print(f"Name in LowerCase: {my_data.name_lower}")
-print(f"Name length: {my_data.name_length} characters")
+print(f"Name in UpperCase: {school_data.name_upper}")
+print(f"Name in LowerCase: {school_data.name_lower}")
+print(f"Name length: {school_data.name_length} characters")
 
 square_root = math.sqrt(Total)
 print(f"Square root of total score: {square_root}")
