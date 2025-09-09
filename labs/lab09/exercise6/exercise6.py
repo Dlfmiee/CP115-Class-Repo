ceddb302bb702652688607a5879e22267ce720c5
Dlfmiee <aidilsahimat07@gmail.com>
2025-09-09 12:09:26 +0800

    Made a change in lab9

--- labs/lab09/exercise6/exercise6.py
+++ labs/lab09/exercise6/exercise6.py
@@ -3,6 +3,11 @@ overtime_hours = int(input())
 is_weekend = input()
 
 # TODO: Your code here
+Position_Hourly = {"Manager": 30, "Supervisor": 20, "Staff": 11, "Intern":8}
 
+if position in Position_Hourly and overtime_hours <= 8:
+    overtime_pay = Position_Hourly * 1.5
+elif position in Position_Hourly and overtime_hours > 8:
+    overtime_pay = Position_Hourly * 2
 
 print(overtime_pay)
