3084c3e48027594601828712c7d0162123185903
Dlfmiee <aidilsahimat07@gmail.com>
2025-09-30 12:53:27 +0800

    Made a change in lab 11

--- labs/lab11/exercise1/exercise1.py
+++ labs/lab11/exercise1/exercise1.py
@@ -2,6 +2,17 @@ num_rounds = int(input())
 
 # TODO: Your code here
 # Use input() inside the loop to get each round's score
+total_score = 0.0
+rounds_processed = 0
+
+for i in range(num_rounds):
+    round_score = float(input())
+    if round_score > 100:
+        round_score = round_score * 1.2   # add 20% bonus
+    total_score += round_score
+    rounds_processed += 1
+
+final_score = total_score
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
--- labs/lab11/exercise2/exercise2.py
+++ labs/lab11/exercise2/exercise2.py
@@ -3,6 +3,17 @@ danger_threshold = float(input())
 
 # TODO: Your code here
 # Use input() inside the loop to get each day's temperature
+danger_days = 0
+total_temp = 0
+
+for i in range(1,num_days+1):
+    temp = float(input())
+    total_temp += temp
+
+    if temp > danger_threshold:
+        danger_days += 1
+
+average_temp = total_temp / num_days
 
 print(danger_days)
 print(f"{average_temp:.1f}")
