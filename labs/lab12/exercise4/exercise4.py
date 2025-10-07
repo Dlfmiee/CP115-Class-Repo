f25d65202e01628b8f80d51b60f0d42a486c918b
Dlfmiee <aidilsahimat07@gmail.com>
2025-10-07 11:49:02 +0800

    Made a change in lab12

--- labs/lab12/exercise4/exercise4.py
+++ labs/lab12/exercise4/exercise4.py
@@ -1,7 +1,24 @@
 score_input = input()
 
 # TODO: Your code here
+passing_count = 0
+failing_count = 0
 
+while score_input != "end":
+    score = float(score_input)
+
+    if score >= 60:
+        passing_count += 1
+    else:
+        failing_count += 1
+    
+    score_input = input()
+
+total_students = passing_count + failing_count
+if total_students > 0:
+    pass_rate = (passing_count / total_students) * 100
+else:
+    pass_rate = 0
 
 print(passing_count)
 print(failing_count)
